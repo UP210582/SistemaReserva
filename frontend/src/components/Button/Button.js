f23844372a06196d9c268451059782c7d36dc0d3
leotencho <leotencho@gmail.com>
2024-07-08 01:48:08 +0000

    JS Components

---
+++ frontend/src/components/Button/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';	
+import PropTypes from 'prop-types';
+
+const Button = (props) => {
+    return(
+        <div className='Button'>
+
+        </div>
+    );
+}
+
+Button.propTypes = {
+    
+}
+
+export default Button;
---
+++ frontend/src/components/CrtRvt/CrtRvt.js
@@ -0,0 +1,16 @@
+import React from 'react';	
+import PropTypes from 'prop-types';
+
+const CrtRvt = (props) => {
+    return(
+        <div className='CrtRvt'>
+
+        </div>
+    );
+}
+
+CrtRvt.propTypes = {
+    
+}
+
+export default CrtRvt;
