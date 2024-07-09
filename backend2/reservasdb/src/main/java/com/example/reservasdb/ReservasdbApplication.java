7b77671d83799ff92ec29685bcc0856d22c44f71
UP210586 <up210586@alumnos.upa.edu.mx>
2024-07-09 16:32:37 -0600

    db

---
+++ backend2/reservasdb/src/main/java/com/example/reservasdb/ReservasdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservasdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasdbApplication.class, args);
+	}
+
+}
---
+++ backend2/reservasdb/src/test/java/com/example/reservasdb/ReservasdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservasdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
