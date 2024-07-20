7ebf20413ebbf37a783c051a886fe96638ddb942
UP210582 <up210582@alumnos.upa.edu.mx>
2024-07-20 16:19:58 -0600

    Ajust Dates, Data

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from '../HomePage';
-import ReservPage from '../ReservPage';
 import ReservPage2 from '../ReservPage2';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/reservation" element={<ReservPage />} />
           <Route path="/reservation2" element={<ReservPage2 />} />
         </Routes>
       </div>
