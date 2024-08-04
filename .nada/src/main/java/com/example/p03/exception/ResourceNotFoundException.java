ac3b9cf4095fe0b8ed36c7257778aeb549f5d0aa
Leonardo Ausencio Martinez Torres <up210582@alumnos.upa.edu.mx>
2024-08-04 11:44:51 -0600

    RemoveDirNda

--- .nada/src/main/java/com/example/p03/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.p03.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- .nada/src/main/java/com/example/p03/repository/PaymentInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.p03.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.p03.model.PaymentInfo;
-
-@Repository
-public interface PaymentInfoRepository extends JpaRepository<PaymentInfo, Long> {
-}
