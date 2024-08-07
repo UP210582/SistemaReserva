1a09412f4ee7e339efcfc5a1bfc84f79201d4238
UP210586 <up210586@alumnos.upa.edu.mx>
2024-08-06 21:06:36 -0600

    db

--- db/mysql-init/init.sql
+++ db/mysql-init/init.sql
@@ -1,5 +1,11 @@
 CREATE DATABASE IF NOT EXISTS restaurant_reservations;
 
+CREATE USER 'restaurant'@'localhost' IDENTIFIED BY 'restaurant';
+
+GRANT ALL PRIVILEGES ON restaurant_reservations.* TO 'restaurant'@'localhost';
+
+flush privileges;
+
 use restaurant_reservations;
 
 CREATE TABLE users (
