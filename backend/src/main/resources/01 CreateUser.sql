5afe78a37eac83e32003e3bb9b62543442e9c2fa
UP210586 <up210586@alumnos.upa.edu.mx>
2024-08-06 22:34:09 -0600

    cambiolocalhostporip2

--- backend/src/main/resources/01 CreateUser.sql
+++ backend/src/main/resources/01 CreateUser.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS restaurant_reservations;
 
-CREATE USER 'restaurant'@'localhost' IDENTIFIED BY 'restaurant';
+CREATE USER 'restaurant'@'%' IDENTIFIED BY 'restaurant';
 
-GRANT ALL PRIVILEGES ON restaurant_reservations.* TO 'restaurant'@'localhost';
+GRANT ALL PRIVILEGES ON restaurant_reservations.* TO 'restaurant'@'%';
 
 flush privileges;
 
